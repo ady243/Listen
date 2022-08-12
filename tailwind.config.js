10f2bb19b3f7f2b877f3cc92a55a1ddfbfdc1df5
masiv <masiviady7@gmail.com>
2022-08-12 23:28:50 +0200

    listen6

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,32 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        "2xl": "1500px",
+      },
+      keyframes: {
+        pulse: {
+          "0%, 100%": {
+            opacity: 0.5,
+          },
+        },
+      },
+      animation: {
+        // pulse: "pulse 4s cubic-bezier(0.4,0,0.6,1) infinte",
+      },
+    },
+    fontFamily: {
+      sans: ["ui-sans-serif", "system-ui"],
+      serif: ["ui-serif", "Georgia"],
+      mono: ["ui-monospace", "SFMono-Regular"],
+      display: ["Oswald"],
+      body: ['"Open Sans"'],
+    },
+  },
+  varants: {
+    extends: {},
+    scrollbar: ["rounded"],
   },
   plugins: [
     require("tailwind-scrollbar-hide"),
