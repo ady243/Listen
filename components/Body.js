3730d3cb083d5010d5cb549435ca7f2b95cbf9fe
masiv <masiviady7@gmail.com>
2022-08-13 01:42:22 +0200

    listen8

--- components/Body.js
+++ components/Body.js
@@ -4,9 +4,13 @@ import React from "react";
 
 function Body() {
   const [search, setSearch] = useState("");
+  const [searchResult, setSearchResult] = useState("");
   return (
     <section>
       <Search Search={setSearch} />
+      <div
+        className="grid grid-cols-2 py-4 overflow-y-scroll scrollbar-hide h-96 lg:grid-cols-2 xl:grid-cols-4 gap-x-4 gap-y-8"
+      ></div>
     </section>
   );
 }
