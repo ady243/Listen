10c42d73132355250808b9f1acc86c0c111636ff
masiv <masiviady7@gmail.com>
2022-08-18 00:08:25 +0200

    mise en place des données pour la barre de recherche

--- components/Body.js
+++ components/Body.js
@@ -1,16 +1,87 @@
 import { Search } from "heroicons-react";
 import { useState } from "react";
 import React from "react";
+import { useEffect } from "react";
+import { useSession } from "next-auth/react";
+import SpotifyWebApi from "spotify-web-api-node";
 
-function Body() {
+function Body({ spotifyApi }) {
+  const { data: session } = useSession();
+  const { accessToken } = session;
   const [search, setSearch] = useState("");
-  const [searchResult, setSearchResult] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  const [newReleases, setNewReleases] = useState([]);
+
+  useEffect(() => {
+    if (!accessToken) return;
+    spotifyApi.setAccessToken(accessToken);
+  }, [accessToken]);
+
+  // search...
+
+  useEffect(() => {
+    if (!search) return setSearchResults([]);
+    if (!accessToken) return;
+
+    spotifyApi.searchTracks(search).then((res) => {
+      setSearchResults(
+        res.body.tracks.items.map((track) => {
+          return {
+            id: track.id,
+            artist: track.artists[0].name,
+            title: track.name,
+            uri: track.uri,
+            albumurl: track.album.images[0].url,
+            popularity: track.popularity,
+          };
+        })
+      );
+    });
+  }, [search, accessToken]);
+
+  console.log(searchResults);
+
+  //New Realease...
+
+  useEffect(() => {
+    if (!accessToken) return;
+
+    spotifyApi.getNewReleases().then((res) => {
+      setNewReleases(
+        res.body.albums.items.map((track) => {
+          return {
+            id: track.id,
+            artist: track.artists[0].name,
+            title: track.name,
+            uri: track.uri,
+            albumurl: track.images[0].url,
+            popularity: track.popularity,
+          };
+        })
+      );
+    });
+  }, [accessToken]);
+  console.log(newReleases);
+
   return (
     <section>
       <Search Search={setSearch} />
       <div
-        className="grid grid-cols-2 py-4 overflow-y-scroll scrollbar-hide h-96 lg:grid-cols-2 xl:grid-cols-4 gap-x-4 gap-y-8"
-      ></div>
+        className="grid grid-cols-2 py-4 overflow-y-scroll 
+      scrollbar-hide h-96 lg:grid-cols-2 xl:grid-cols-4 gap-x-4 gap-y-8"
+      >
+        {searchResults.length === 0
+          ? newReleases
+              .slice(0, 4)
+              .map((track) => (
+                <Poster
+                  key={track.id}
+                  track={track}
+                  chooseTrack={chooseTrack}
+                />
+              ))
+          : searchResults.slice(0, 4).map((track) => <Poster />)}
+      </div>
     </section>
   );
 }
--- components/Dashboard.js
+++ components/Dashboard.js
@@ -1,14 +1,18 @@
+import SpotifyWebApi from "spotify-web-api-node";
 import Body from "./Body";
 import Right from "./Right";
 import Sidebar from "./Sidebar";
 import Search from "./Search";
 
-const spotifyApi = new spotifyApi({});
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.SPOTIFY_CLIENT_ID,
+});
+spotifyApi;
 function Dashboard() {
   return (
     <main>
       <Sidebar />
-      <Body />
+      <Body spotifyApi={spotifyApi} />
       <Right />
       <Search />
     </main>
