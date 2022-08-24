ee01aeeff3179e72474501c38f90725fb393ded8
masiv <masiviady7@gmail.com>
2022-08-24 15:30:44 +0200

    conti,uation du style et de l'appel pour afficher les musics

--- components/Poster.js
+++ components/Poster.js
@@ -1,13 +1,30 @@
 import React from "react";
+import { IoPause, IoPlay } from "react-icons/io5";
 
-function Poster() {
+function Poster({ track }) {
   return (
     <div
       className="w-[260] h-[360] rounded-none overflow-hidden relative text-white
     cursor-pointer hover:scale-105 hover:text-white/100 transition
      duration-200 ease-out group-mx-auto
     "
-    ></div>
+    >
+      <img
+        src={track.albumUrl}
+        alt=""
+        className="h-full w-full absolute inset-0
+      object-contain groupe-hover:opacity-100"
+      />
+      <div className="absolute bottom-10 inset-x-0 ml-4 flex items-center space-x-3.5">
+        <div
+          className="h-10 w-10 bg-[#42cbcf] rounded-full flex items-center justify-center
+        group-hover:bg-[#42cbcf] flex-shrink-0"
+        >
+          <IoPause className="text-xl" />
+          <IoPlay className="text-l ml-[1px]" />
+        </div>
+      </div>
+    </div>
   );
 }
 
